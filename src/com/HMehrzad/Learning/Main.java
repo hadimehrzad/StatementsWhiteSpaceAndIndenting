31d139b1538df023b6327d3eaab648415f08453e
hadimehrzad <61123461+hadimehrzad@users.noreply.github.com>
2020-02-27 21:57:16 +0330

    Ex9: Statements WhiteSpace And Indenting

---
+++ src/com/HMehrzad/Learning/Main.java
@@ -0,0 +1,22 @@
+package com.HMehrzad.Learning;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        int             myVariable
+
+                =
+
+                50
+
+                ;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+        System.out.println("This is" +
+                " another" +
+                "still more.");
+        int anotherVaiable = 50; myVariable--; myVariable++; System.out.println("This is another one");
+    }
+}
